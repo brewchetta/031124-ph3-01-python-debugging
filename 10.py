5d79fd29f6f3a483ec043c3d69f6d583a63ef97e
brewchetta <chett.tiller@gmail.com>
2024-04-22 11:08:10 -0400

    updated faker error message

--- 10.py
+++ 10.py
@@ -12,7 +12,9 @@ person = {
 print( person )
 
 '''
-File "/home/chett/031124/p3/01-python-debugging/10.py", line 1, in <module>
-    from faker import Faker
-ModuleNotFoundError: No module named 'faker'
+File "/home/chett/031124/p3/01-python-debugging/10.py", line 9, in <module>
+    'phone': fake.phone()
+  File "/home/chett/.pyenv/versions/3.8.13/lib/python3.8/site-packages/faker/proxy.py", line 130, in __getattr__
+    return getattr(self._factories[0], attr)
+AttributeError: 'Generator' object has no attribute 'phone'
 '''
